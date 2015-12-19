2bb3d1eaac938e0c17c49956d26184ccc1d4e7d1
andrestroy <camusdeacuariogt@gmail.com>
2015-12-19 09:31:47 -0400

    Adding Reflection Demo

---
+++ CSharp_JumpStart/ReflectionDemo/BattleGround.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class BattleGround
+    {
+        public void Init()
+        {
+            //TODO initialize all the game configuration
+        }
+    }
+}
---
+++ CSharp_JumpStart/ReflectionDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BattleGround gameBoard = new BattleGround();
+            ListMethods(gameBoard.GetType());
+        }
+
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("============= Methods ===========");
+            MethodInfo[] mi = t.GetMethods();
+            foreach (var item in mi)
+            {
+                Console.WriteLine("->{0}", item.Name);
+            }
+            Console.WriteLine();
+        }
+    }
+}
